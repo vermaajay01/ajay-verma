c1f222b90031b427e999241b884859113426098e
vermaajay01 <vermaajay10125@gmail.com>
2025-01-03 14:03:25 +0530

    Create 02_datatype.js

---
+++ 02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3 ) //we are using nodejs, not browser
+
+console.log(3+3); //code readability should be high
+console.log("ajay");
+
+let name = "ajay"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "ajay");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
