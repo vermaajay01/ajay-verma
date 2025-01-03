a0cc00c0263680bbbc474bffb2aa10a3f466c770
vermaajay01 <vermaajay10125@gmail.com>
2025-01-03 14:04:20 +0530

    Create 03_conversionOperations.js

---
+++ 03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = "ajay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false
+// "" => false
+// "ajay" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
